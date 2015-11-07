73efe32820aed4a705b7eac596d3792c770851c9
Antoni Virós Martín <ani300@gmail.com>
2015-11-07 09:52:43 +0100

    Added first networking to app

--- android/app/src/main/java/com/finapps/pep/InsertTaskActivity.java
+++ android/app/src/main/java/com/finapps/pep/InsertTaskActivity.java
@@ -2,19 +2,39 @@ package com.finapps.pep;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.os.AsyncTask;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class InsertTaskActivity extends AppCompatActivity {
 
@@ -26,7 +46,7 @@ public class InsertTaskActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
-        TextView tv3 = (TextView) findViewById(R.id.editText3);
+        EditText tv3 = (EditText) findViewById(R.id.editText3);
         tv3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,6 +100,37 @@ public class InsertTaskActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_send:
                 // Send the request to the server
+                EditText te1 = (EditText) findViewById(R.id.editText);
+                EditText te2 = (EditText) findViewById(R.id.editText2);
+                EditText te3 = (EditText) findViewById(R.id.editText3);
+                Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+                Date date = null;
+                DateFormat formatter = new SimpleDateFormat("dd/mm/yyyy");
+                try {
+                    date = (Date)formatter.parse(te3.getText().toString());
+                }
+                catch (ParseException e) {
+
+                }
+
+                String spinstr = spinner.toString();
+                if (spinstr.equals("None")) {
+                    spinstr = "0";
+                }
+                else if (spinstr.equals("Day")) {
+                    spinstr = "1";
+                }
+                else if (spinstr.equals("Month")) {
+                    spinstr = "2";
+                }
+                else {
+                    spinstr = "3";
+                }
+
+                new AddObjectiveTask().execute(te1.getText().toString(),
+                        String.valueOf(date.getTime()), te2.getText().toString(),
+                        spinstr);
                 return true;
 
             default:
@@ -88,5 +139,98 @@ public class InsertTaskActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private class AddObjectiveTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return addObjective(params);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Log.v("LO", result);
+            if (result.equals("Success")) {
+                Toast.makeText(getApplicationContext(), "WOLOLO", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String addObjective(String... info) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 4096;
+
+        try {
+            URL url = new URL("http://192.168.10.11/objective");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(60000);
+            conn.setConnectTimeout(75000);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Accept", "text/html");
+            conn.setRequestMethod("POST");
+
+            //Create JSONObject here
+            List<Pair<String, String>> params = new ArrayList<>();
+            params.add(new Pair<>("name", info[0]));
+            params.add(new Pair<>("date", info[1]));
+            params.add(new Pair<>("value", info[2]));
+            params.add(new Pair<>("period", info[3]));
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(getQuery(params));
+            wr.flush();
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("LO", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            return readIt(is, len);
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private String getQuery(List<Pair<String, String>> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        for (Pair<String, String> pair : params)
+        {
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(pair.first, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(pair.second, "UTF-8"));
+        }
+        return result.toString();
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException {
+        Reader reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
     }
 }
